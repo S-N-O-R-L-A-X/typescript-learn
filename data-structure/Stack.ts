a6bb9c1986850b1bc3c47607ea5eb1b487d7b81a
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2022-12-06 23:04:51 +0800

    feat: add stack

---
+++ data-structure/Stack.ts
@@ -0,0 +1,30 @@
+class Stack<T> {
+    stack: T[];
+    constructor() {
+        this.stack = [];
+    }
+
+    get size(): number {
+        return this.stack.length;
+    }
+
+    isEmpty(): boolean {
+        return this.size === 0;
+    }
+
+    push(elem: T): void {
+        this.stack.push(elem);
+    }
+
+    pop(): T {
+        return this.stack.pop();
+    }
+
+    peek(): T {
+        return this.stack[this.size - 1];
+    }
+
+    get(index: number) {
+        return this.stack[index];
+    }
+}
