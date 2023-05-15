2adeabc87deec33ac7d3c1920233e2cf2981f8df
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2023-05-15 23:18:26 +0800

    fix: fix priority queue

--- data-structure/PriorityQueue.ts
+++ data-structure/PriorityQueue.ts
@@ -1,67 +1,67 @@
 class PriorityQueue<T> {
-    arr: T[];
-    comparator: Function;
-    constructor(comparator?: Function) {
-        this.arr = [];
-        this.comparator = comparator || ((a, b) => a - b);;
+  arr: T[];
+  comparator: Function;
+  constructor(comparator?: Function) {
+    this.arr = [];
+    this.comparator = comparator || ((a, b) => a - b);;
+  }
+
+  pushDown(k): void {
+    while (k * 2 + 1 < this.size) {
+      const left_son = k * 2 + 1, right_son = k * 2 + 2;
+      let son = left_son;
+
+      if (right_son < this.size && this.comparator(this.arr[right_son], this.arr[left_son]) > 0) { //choose left or right
+        son = right_son;
+      }
+
+      if (this.comparator(this.arr[k], this.arr[son]) > 0) {
+        break;
+      }
+      this.swapByIdx(son, k);
+      k = son;
     }
+  }
 
-    pushDown(k): void {
-        while (k * 2 + 1 < this.size) {
-            const left_son = k * 2 + 1, right_son = k * 2 + 2;
-            let son = left_son;
+  isEmpty(): boolean {
+    return this.arr.length === 0;
+  }
 
-            if (right_son < this.size && this.comparator(this.arr[left_son], this.arr[right_son]) > 0) { //choose left or right
-                son = right_son;
-            }
+  swapByIdx(i: number, j: number): void {
+    const tmp = this.arr[i];
+    this.arr[i] = this.arr[j];
+    this.arr[j] = tmp;
+  }
 
-            if (this.comparator(this.arr[son], this.arr[k]) > 0) {
-                break;
-            }
-            this.swapByIdx(son, k);
-            k = son;
-        }
+  push(element: T): void {
+    let son = this.size;
+    this.arr[this.size] = element;
+    while (son) {
+      const child = (son - 1) >> 1;
+      if (this.comparator(this.arr[son], this.arr[child]) > 0)
+        this.swapByIdx(son, child);
+      son = child;
     }
+  }
 
-    isEmpty(): boolean {
-        return this.arr.length === 0;
+  pop(): T {
+    if (this.isEmpty()) {
+      return undefined;
     }
+    if (this.size === 1) {
+      return this.arr.pop();
+    }
+    this.swapByIdx(0, this.size - 1);
+    const ret = this.arr.pop();
+    this.pushDown(0);
+    return ret;
+  }
 
-    swapByIdx(i: number, j: number): void {
-        const tmp = this.arr[i];
-        this.arr[i] = this.arr[j];
-        this.arr[j] = tmp;
-    }
+  top(): T {
+    return this.arr[0];
+  }
 
-    push(element: T): void {
-        this.arr[this.size] = element;
-        let son = this.size;
-        while (son) {
-            const child = (son - 1) >> 1;
-            if (this.comparator(this.arr[son], this.arr[child]) > 0)
-                this.swapByIdx(son, child);
-            son = child;
-        }
-    }
-
-    pop(): T {
-        if (this.isEmpty()) {
-            return undefined;
-        }
-        if (this.size === 1) {
-            return this.arr.pop();
-        }
-        this.swapByIdx(0, this.size - 1);
-        const ret = this.arr.pop();
-        this.pushDown(0);
-        return ret;
-    }
-
-    top(): T {
-        return this.arr[0];
-    }
-
-    get size() {
-        return this.arr.length;
-    }
+  get size() {
+    return this.arr.length;
+  }
 }
