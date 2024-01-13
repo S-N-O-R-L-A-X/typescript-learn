a9b0aeb6a84cb18fc9754fec11e9c64d6c79fd82
S-N-O-R-L-A-X <snorlax.xu@outlook.com>
2024-01-13 23:12:17 +0800

    feat: add bitwise actions

--- algorithm/bitwiseFunctions.ts
+++ algorithm/bitwiseFunctions.ts
@@ -11,3 +11,21 @@ function bitCount(num: number): number {
 	num = num + (num >>> 16);
 	return num & 0x3f;
 }
+
+/**
+ * 
+ * @param num 
+ * @returns the num after removing the lowest 1 in num
+ */
+function removeLowest1(num: number): number {
+	return num & (num - 1);
+}
+
+/**
+ * 
+ * @param num 
+ * @returns the num after setting the lowest 0 to 1 in num
+ */
+function setLowest0(num: number): number {
+	return num | (num + 1);
+}
