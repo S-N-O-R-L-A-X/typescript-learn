e65db222dfce96ee129b7952143ea752903b4126
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2024-03-26 22:00:47 +0800

    feat: add dijkstra

--- algorithm/dijkstra.ts
+++ algorithm/dijkstra.ts
@@ -1,3 +1,4 @@
+import PriorityQueue from "../data-structure/PriorityQueue";
 const inf = 1e9 + 7;
 function dijkstra1(edges: number[][], n: number, starting: number) {//n is the number of node 
     const routes = new Array<number>(n).fill(0).map(() => new Array<number>(n).fill(inf));
@@ -18,4 +19,31 @@ function dijkstra1(edges: number[][], n: number, starting: number) {//n is the n
     }
     const ret = Math.max(...dst);
     return ret == inf ? -1 : ret;//perhaps there is a vertex that cannot be reached
+}
+
+function dijkstra2(edges: [number, number, number][], n: number, starting: number): number[] {
+    const routes: [number, number][][] = Array.from({ length: n }, () => []);
+    for (const edge of edges) {
+        // record present edge
+        routes[edge[0]].push([edge[1], edge[2]]);
+    }
+
+    const dst: number[] = Array.from({ length: n }, () => Infinity);
+    dst[starting] = 0;
+    const pq=new PriorityQueue<[number,number]>((a,b)=>a[0]-b[0]);
+    // pq stores {distance, target}
+    pq.push([0, starting]);
+
+    while (pq.isEmpty()) {
+        const node: [number, number] = pq.pop();
+        const dis: number = node[0], x: number = node[1];
+        if (dst[x] > dis) {
+            dst[x] = dis;
+            for (const [neighbor, weight] of routes[x]) {
+                pq.push([dis + weight, neighbor]);
+            }
+        }
+    }
+
+    return dst;
 }
